13af430b5fd60caa695ede60be71880a912ed09e
ZEnd <om99305@gmail.com>
2025-04-04 00:49:57 +0700

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,8 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
-
 export default config;
+
+
---
+++ src/Component/Common/BentoPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BentoPage = () => {
+  return (
+    <div>BentoPage</div>
+  )
+}
+
+export default BentoPage
